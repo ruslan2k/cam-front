d0cb7022c5c90da884db6fc38f1ca26b486033f9
Ruslan Karachun <free.changer@gmail.com>
2016-05-01 00:29:51 +0300

    Add ENV

--- yii/web/index.php
+++ yii/web/index.php
@@ -7,6 +7,11 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+if (YII_ENV === 'dev' and file_exists(__DIR__ . '/../.env')) {
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+    $dotenv->load();
+}
+
 $config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
