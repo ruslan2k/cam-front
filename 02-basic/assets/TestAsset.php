408e337a32c708c6a90cc3713b1adb8132ff0fa2
Ruslan Karachun <free.changer@gmail.com>
2016-05-05 17:00:41 +0300

    Save assets

---
+++ 02-basic/assets/TestAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class TestAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+    ];
+    public $js = [
+        '/js/test.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
